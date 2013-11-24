b7d8a713c04eb85da9bb4df1ef95ece73d2292aa
Lachlan Plant <lp11qf@brocku.ca>
2013-11-24 15:32:48 -0500

    Stuff and things
    bop

--- BattleBunniesEclipse/src/ca/fluffybunny/battebunnies/activities/MainActivity.java
+++ BattleBunniesEclipse/src/ca/fluffybunny/battebunnies/activities/MainActivity.java
@@ -1,40 +1,60 @@
 package ca.fluffybunny.battebunnies.activities;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import ca.fluffybunny.battlebunnies.R;
 
 public class MainActivity extends Activity {
 
-    @Override
+	private static final int REQUEST_CONNECT_DEVICE = 1;
+	private BluetoothAdapter mBluetoothAdapter = null;
+	@Override
+	
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
         return true;
     }
-
-
-    @Override
+    
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        switch (item.getItemId()) {
+        case R.id.scan:
+            // Launch the DeviceListActivity to see devices and do scan
+        	ensureDiscoverable();
+            Intent serverIntent = new Intent(this, DeviceListActivity.class);
+            startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
             return true;
         }
-        return super.onOptionsItemSelected(item);
+        return false;
     }
+    
+    private void ensureDiscoverable() {
+        if (mBluetoothAdapter.getScanMode() !=
+            BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoverableIntent);
+        }
+    }
+        
+    
+
+
 
 
     /**
